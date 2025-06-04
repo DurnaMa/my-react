b23388d73009bf75b7afc0b787f992f5c3eef71b
Mahir Durna <durna.m@outlook.de>
2025-06-04 22:57:31 +0200

    class Components

--- my-react/src/components/App.jsx
+++ my-react/src/components/App.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 
 export class App extends React.Component {
-  render() {
-    const { title, name } = this.props
-    return <div title={title}>Halllo Component {name} !</div>;
+  state = {
+    counterValue: 3,
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        counterValue: Math.max(0, this.state.counterValue -1),
+      })
+    }, 1000);
   }
+
+  render() {
+    return <strong>Contdown = {this.state.counterValue}</strong>;
+  }
+
+  // render() {
+  //   const { title, nameObj } = this.props;
+  //   return (
+  //     <div title={title}>
+  //       Halllo Component {nameObj.prename} {nameObj.lastname} !
+  //     </div>
+  //   );
+  // }
 }
--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-//import './index.css';
-import {App} from './components/App';
-//import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const nameObj = {
+  prename: "Mahir",
+  lastname: "Durna",
+};
 root.render(
   <React.StrictMode>
-    <App title="test" name="linkdIn"/>
+    <App title="test" name="linkdIn" nameObj={nameObj} />
   </React.StrictMode>
 );
-
